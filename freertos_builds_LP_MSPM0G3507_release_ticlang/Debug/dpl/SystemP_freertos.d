4b635588f1e626f8b5a07b9382174d4a573d47e0
AiliuCheng58 <gaosongdeng58@gmail.com>
2026-06-02 08:38:48 +0800

    Apply gitignore and remove ignored files

--- freertos_builds_LP_MSPM0G3507_release_ticlang/Debug/dpl/SystemP_freertos.d
+++
@@ -1,6 +0,0 @@
-# FIXED
-
-dpl/SystemP_freertos.o: \
- D:/AiliuCheng/work/DIAN_RACE/TiSDK/mspm0_sdk_2_10_00_04/kernel/freertos/dpl/SystemP_freertos.c \
- D:/AiliuCheng/work/DIAN_RACE/TiSDK/mspm0_sdk_2_10_00_04/source/ti/drivers/dpl/SystemP.h
-D:/AiliuCheng/work/DIAN_RACE/TiSDK/mspm0_sdk_2_10_00_04/source/ti/drivers/dpl/SystemP.h:
